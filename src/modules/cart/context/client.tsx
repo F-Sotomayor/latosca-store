ba0b8a59a1c786cd8948ad7b5e719a8c592b2201
F-Sotomayor <facundosotomayor95@hotmail.com>
2024-07-11 14:06:33 -0300

    build?

--- src/modules/cart/context/client.tsx
+++ src/modules/cart/context/client.tsx
@@ -65,7 +65,7 @@ function CartProviderClient({
           // Update the quantity of the existing item in cart
           const currentQuantity = item.options?.[value.category]?.[0]?.quantity || 0;
 
-          item.options[value.category][0].quantity = currentQuantity + quantityToAdd;
+          item?.options?[value.category][0].quantity = currentQuantity + quantityToAdd;
 
           // Update cart with modified item
           cart.set(key, item);
