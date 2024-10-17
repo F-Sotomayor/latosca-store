f0cea15227d85f015e9811683b9c4f251c615201
F-Sotomayor <59397328+F-Sotomayor@users.noreply.github.com>
2024-10-17 12:08:47 -0300

    Update utils.ts

--- src/modules/cart/utils.ts
+++ src/modules/cart/utils.ts
@@ -21,7 +21,7 @@ export function getCartItemPrice(item: CartItem): number {
 
 export function getCartTotal(cart: Cart): number {
   return Array.from(cart.values()).reduce(
-    (total, item) => total + getCartItemPrice(item) + (item.deliveryPrice > 1 ? 600 : 0),
+    (total, item) => total + getCartItemPrice(item) + (item.deliveryPrice > 1 ? 700 : 0),
     0,
   );
 }
